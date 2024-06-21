c621a0491df614a1636287b377508acb70e20829
Hirbod03 <shoss2@uwo.ca>
2024-06-21 12:53:59 -0400

    added output handling options for user

--- AutoUnfollower.py
+++ AutoUnfollower.py
@@ -1,7 +1,7 @@
 import csv
 from ensta import Mobile 
 
-momobile = Mobile("USERNAME", "PASSWORD")
+mobile = Mobile("USERNAME", "PASSWORD")
 
 followers = mobile.followers("USERNAME")
 followings = mobile.followings("USERNAME")
@@ -12,10 +12,28 @@ followers = mobile.followers("USERNAME", next_cursor=followers.next_cursor)
 # Convert followers and followings to sets for faster lookup
 followers_usernames = set(follower.username for follower in followers.list)
 followings_usernames = set(following.username for following in followings.list)
+non_followers = followings_usernames - followers_usernames
 
 print("Following and Follower lists processed successfully!")
+# Ask the user what they want to do with the result
+print("What would you like to do with the users who don't follow back?")
+print("1: Unfollow them")
+print("2: Print their usernames")
+print("3: Do nothing")
+user_choice = input("Enter your choice (1, 2, or 3): ")
 
-# Unfollow users who don't follow back
-for username in followings_usernames:
-    if username not in followers_usernames:
+if user_choice == '1':
+    # Unfollow users who don't follow back
+    for username in non_followers:
         mobile.unfollow(username)
+    print(f"Unfollowed {len(non_followers)} users.")
+elif user_choice == '2':
+    # Print usernames of users who don't follow back
+    print("Users who don't follow back:")
+    for username in non_followers:
+        print(username)
+elif user_choice == '3':
+    # Do nothing
+    print("No action taken.")
+else:
+    print("Invalid choice. Exiting.")
