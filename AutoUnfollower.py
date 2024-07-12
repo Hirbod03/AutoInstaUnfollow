2ec61169dc913e14f45228fb0cf31926709bafc8
Hirbod03 <shoss2@uwo.ca>
2024-07-12 14:37:59 -0400

    fixed bugs. mobile tool not well developed. had to switch to web. please inform me of any further bugs :)

--- AutoUnfollower.py
+++ AutoUnfollower.py
@@ -1,49 +1,52 @@
 import csv
-from ensta import Mobile 
+from ensta import Web 
 
-username = ""
-password = ""
+username = "INSET_USERNAME_HERE"
+password = "INSET_PASSWORD_HERE"
 
-mobile = 0
+web = None
 
 try:
-    mobile = Mobile(username, password)
+    web = Web(username, password)
 except Exception as e:
-    print("Login failed. Please check your credentials.")
+    print("Login failed.")
     print(f"Error details: {e}")
     exit()
 
-followers = mobile.followers(username)
-followings = mobile.followings(username)
+followers = web.followers(username)
+followings = web.followings(username)
 
 # Fetching next chunk
-followers = mobile.followers(username, next_cursor=followers.next_cursor)
+# followers = mobile.followers(username, next_cursor=followers.next_cursor)
 
 # Convert followers and followings to sets for faster lookup
-followers_usernames = set(follower.username for follower in followers.list)
-followings_usernames = set(following.username for following in followings.list)
+followers_usernames = set(follower.username for follower in followers)
+followings_usernames = set(following.username for following in followings)
 non_followers = followings_usernames - followers_usernames
 
 print("Following and Follower lists processed successfully!")
-# Ask the user what they want to do with the result
 print("What would you like to do with the users who don't follow back?")
 print("1: Unfollow them")
 print("2: Print their usernames")
 print("3: Do nothing")
+
 user_choice = input("Enter your choice (1, 2, or 3): ")
 
 if user_choice == '1':
     # Unfollow users who don't follow back
     for username in non_followers:
-        mobile.unfollow(username)
+        web.unfollow(username)
     print(f"Unfollowed {len(non_followers)} users.")
+
 elif user_choice == '2':
     # Print usernames of users who don't follow back
     print("Users who don't follow back:")
     for username in non_followers:
         print(username)
+
 elif user_choice == '3':
     # Do nothing
     print("No action taken.")
+
 else:
     print("Invalid choice. Exiting.")
